3bbce75c7a5d12368e2eeac8f3fa70b1f07cb6ab
Aleksandr Mami <Aleksandr_Mami@epam.com>
2021-11-23 00:21:45 +0300

    JUnit test demo 1

---
+++ src/test/java/JUnitExampleTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JUnitExampleTest {
+
+    @Test
+    public void demoMethodUnderTest(){
+
+        assertTrue(true);
+
+
+    }
+
+}
