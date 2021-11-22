d64b0ba30ea79de045ea407c7f76563df85ec51b
Aleksandr Mami <Aleksandr_Mami@epam.com>
2021-11-23 00:43:21 +0300

    @BeforeAll setup

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,10 +1,18 @@
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    Calculator calculator = new Calculator();
+   public static Calculator calculator;
+
+    @BeforeAll
+    public static void setup() {
+        calculator = new Calculator();
+    }
 
     @Test
     public void add_success() {
